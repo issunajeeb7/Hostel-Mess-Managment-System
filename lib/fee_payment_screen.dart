a9dce3cf815d65b9aae664f532dab3539ec1740f
issunajeeb <issunajeeb7@gmail.com>
2023-12-16 12:15:23 +0530

    Fee Payment Completed

--- lib/fee_payment_screen.dart
+++ lib/fee_payment_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FeePaymentScreen extends StatefulWidget {
   @override
@@ -12,8 +14,11 @@ class _FeePaymentScreenState extends State<FeePaymentScreen> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
   late Razorpay _razorpay;
-  bool feeStatus = false; // Declare feeStatus at the class level
+  bool feeStatus = false;
   int remainingFee = 0;
+  int totalVouchers = 0;
+  final DateFormat _dateFormat = DateFormat('yyyy-MM-dd');
+  int usedVouchers = 0;
 
   @override
   void initState() {
@@ -22,6 +27,62 @@ class _FeePaymentScreenState extends State<FeePaymentScreen> {
     _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
     _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
     _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+    _checkAndUpdateFeeStatus();
+  }
+
+  Future<void> _checkAndUpdateFeeStatus() async {
+    try {
+      // Get the current date
+      DateTime currentDate = DateTime.now();
+      String currentDateString = _dateFormat.format(currentDate);
+
+      // Check if it's the beginning of the month (day 1)
+      if (currentDate.day == 1) {
+        // Set feeStatus to false for all users
+        QuerySnapshot usersSnapshot =
+            await _firestore.collection('users').get();
+        for (QueryDocumentSnapshot userDoc in usersSnapshot.docs) {
+          await userDoc.reference.update({'feeStatus': false});
+        }
+
+        // Log that feeStatus has been updated
+        print('FeeStatus updated to false for all users on $currentDateString');
+      }
+
+      // Fetch user data to calculate remainingFee and totalVouchers
+      User? currentUser = _auth.currentUser;
+      if (currentUser != null) {
+        DocumentSnapshot userData =
+            await _firestore.collection('users').doc(currentUser.uid).get();
+
+        // Calculate remainingFee based on your logic (replace this with your logic)
+        int totalFee = 2400;
+        List<dynamic> generatedVouchersData =
+            userData['generatedVouchers'] ?? [];
+        usedVouchers = 0;
+
+        DateTime now = DateTime.now();
+        String currentMonthYear = '${now.year}-${now.month}';
+        for (var voucherString in generatedVouchersData) {
+          List<String> voucherComponents = voucherString.split('-');
+          if (voucherComponents.length == 4 &&
+              voucherComponents[0] == now.year.toString() &&
+              voucherComponents[1] == now.month.toString()) {
+            usedVouchers++;
+          }
+        }
+        setState(() {
+          remainingFee = totalFee - (usedVouchers * 30);
+        });
+
+        // Update feeStatus based on the backend value
+        feeStatus = userData['feeStatus'] ?? false;
+
+        print('Number: $usedVouchers');
+      }
+    } catch (e) {
+      print('Error checking and updating feeStatus: $e');
+    }
   }
 
   @override
@@ -32,9 +93,8 @@ class _FeePaymentScreenState extends State<FeePaymentScreen> {
 
   void _launchRazorpay() {
     var options = {
-      'key': 'rzp_test_7mJ2dRxRtudD36', // Replace with your API key
-      'amount': remainingFee *
-          100, // Amount is in the smallest currency unit (like paise for INR)
+      'key': 'rzp_test_7mJ2dRxRtudD36',
+      'amount': remainingFee * 100,
       'name': 'Hostel Fee Payment',
       'description': 'Monthly Hostel Fee',
       'prefill': {'contact': '1234567890', 'email': 'test@example.com'},
@@ -90,72 +150,85 @@ class _FeePaymentScreenState extends State<FeePaymentScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            StreamBuilder<DocumentSnapshot>(
-              stream: _firestore
-                  .collection('users')
-                  .doc(_auth.currentUser!.uid)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return CircularProgressIndicator();
-                }
-
-                var userData = snapshot.data!.data() as Map<String, dynamic>;
-                feeStatus = userData['feeStatus'] ?? false;
-                int totalFee = 2400;
-
-                List<dynamic> generatedVouchersData =
-                    userData['generatedVouchers'] ?? [];
-                int usedVouchers = 0;
-
-// Assuming 'generatedVouchers' is an array of strings with the format "yyyy-MM-dd-MealType"
-                DateTime now = DateTime.now();
-                String currentMonthYear = '${now.year}-${now.month}';
-                // print("Date: $currentMonthYear");
-                for (var voucherString in generatedVouchersData) {
-                  // Split the voucher string into components
-                  List<String> voucherComponents = voucherString.split('-');
-                  print('Voucher String: $voucherString');
-                  print('Voucher Components: $voucherComponents');
-
-                  // Check if the voucher is from the current month and year
-                  if (voucherComponents.length == 4 &&
-                      voucherComponents[0] == now.year.toString() &&
-                      voucherComponents[1] == now.month.toString()) {
-                    usedVouchers++;
-                    print('Added voucher: $voucherString');
-                  } else {
-                    print('Skipped voucher: $voucherString');
-                  }
-                }
-                // print("Number of vouchers: $usedVouchers");
-                remainingFee = totalFee - (usedVouchers * 30);
-
-                return Column(
-                  children: [
-                    Text('Remaining Fee: Rs $remainingFee'),
-                    SizedBox(height: 20),
-                    Text('Total Vouchers generated this Month: $usedVouchers'),
-                  ],
-                );
-              },
+            Text(
+              'Pending Fees',
+              style: GoogleFonts.nunitoSans(
+                fontSize: 25.0,
+                fontWeight: FontWeight.normal,
+              ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 50),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 35, horizontal: 70),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: const Color(0xFFFFF9EA),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 0,
+                    blurRadius: 4,
+                    offset: const Offset(0, 6),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    '₹$remainingFee',
+                    style: GoogleFonts.nunitoSans(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Total Vouchers: $usedVouchers',
+                    style: GoogleFonts.nunitoSans(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 200),
             ElevatedButton(
-              onPressed: () {
-                // Check if fee is not already paid
-                if (!feeStatus) {
-                  _launchRazorpay();
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text("Fee already paid for this month.")));
-                }
-              },
-              child: const Text('Pay Now'),
+              onPressed: _isButtonEnabled()
+                  ? feeStatus
+                      ? null
+                      : _launchRazorpay
+                  : null,
+              style: ElevatedButton.styleFrom(
+                primary: _isButtonEnabled()
+                    ? feeStatus
+                        ? Colors.grey
+                        : const Color(0xFFFBC32C)
+                    : Colors.grey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                shadowColor: Colors.black.withOpacity(1.0),
+                elevation: 4,
+                minimumSize: const Size(325, 55),
+              ),
+              child: Text(
+                feeStatus ? 'Already Paid' : 'Pay Now',
+                style: GoogleFonts.nunitoSans(
+                  fontSize: 28,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  bool _isButtonEnabled() {
+    DateTime now = DateTime.now();
+    return now.day >= 28 && now.day <= 30;
   }
 }
